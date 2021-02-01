79a813156c12d646074ca2e8464d88670befb2f6
Oisin Allen <oisin.allen@mydit.ie>
2021-02-01 21:29:02 +0000

    test

---
+++ app/src/main/java/com/example/eyepath/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.eyepath;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import camp.visual.gazetracker.GazeTracker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+    String email;
+    String password;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        Log.i(TAG, "sdk version : " + GazeTracker.getVersionName());
+    }
+
+    public void signUp(View view) {
+
+        EditText emailText = findViewById(R.id.emailText);
+        EditText passwordText = findViewById(R.id.passwordText);
+
+        email = emailText.getText().toString();
+        password = passwordText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d("MyActivity", "createUserWithEmail:success");
+                            mUser = mAuth.getCurrentUser();
+                            String userID = mUser.getUid();
+                            DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+                            User user = new User(email);
+                            db.child("User").child(userID).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(MainActivity.this, "User Signed Up", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            Intent intent = new Intent(getApplicationContext(), ConfigureEyeGaze.class);
+                            startActivity(intent);
+                        } else {
+                            Log.w("MyActivity", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void signIn(View view) {
+
+        EditText emailText = findViewById(R.id.emailText);
+        EditText passwordText = findViewById(R.id.passwordText);
+
+        email = emailText.getText().toString();
+        password = passwordText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "User signed in", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), ConfigureEyeGaze.class);
+                            startActivity(intent);
+                        } else {
+                            Log.w("MySignin", "SignInUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/eyepath/User.java
@@ -0,0 +1,17 @@
+package com.example.eyepath;
+
+public class User {
+    String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
